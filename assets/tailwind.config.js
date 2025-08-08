43d407520e7ad551bb66037777f30a5ed697921b
Young Gyu Park <ygpark2@gmail.com>
2025-08-08 13:59:57 +0900

    Add Tailwind configuration file

---
+++ assets/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./js/**/*.js",
+    "../lib/ain_com_booking_web.ex",
+    "../lib/ain_com_booking_web/**/*.*ex"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- config/config.exs
+++ config/config.exs
@@ -51,6 +51,13 @@ config :esbuild,
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
 
+config :tailwind,
+  version: "3.4.4",
+  default: [
+    args: ~w(--config=tailwind.config.js --input=css/app.css --output=../priv/static/assets/app.css),
+    cd: Path.expand("../assets", __DIR__)
+  ]
+
 config :logger, backends: [:console, Sentry.LoggerBackend]
 
 config :phoenix, :json_library, Jason
