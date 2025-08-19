e57c348aef82d5805b5982bd66dc8399b1c19336
Young Gyu Park <ygpark2@gmail.com>
2025-08-19 23:48:28 +0900

    Integrate TailAdmin layout with Alpine

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     "../lib/ain_com_booking_web/**/*.*ex"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          25: '#f2f7ff',
+          50: '#ecf3ff',
+          100: '#dde9ff',
+          200: '#c2d6ff',
+          300: '#9cb9ff',
+          400: '#7592ff',
+          500: '#465fff',
+          600: '#3641f5',
+          700: '#2a31d8',
+          800: '#252dae',
+          900: '#262e89',
+          950: '#161950'
+        }
+      }
+    },
   },
   plugins: [],
 };
